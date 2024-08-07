68f2ef2376aa8ae3ec6b01211fd3890c13eef7a4
souravverma12 <sourav7889509712@gmail.com>
2024-08-07 22:46:06 +0530

    done

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://sourav7889509712:eReSIFiAXDH2diJC@cluster0.z09uy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
